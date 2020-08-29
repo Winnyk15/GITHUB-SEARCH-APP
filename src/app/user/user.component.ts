40bc82bee1236c9c08d3323f1d9247765254f76c
Winnyk15 <kahendahwinnie@gmail.com>
2020-08-29 15:55:29 +0300

    add initial commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {UsersService} from '../user-service/users.service';
+
+import {UserRequestService} from '../user-http/user-request.service';
+import {User} from '../user-class/user';
+import {Repository} from '../repository-class/repository'
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+  providers:[UsersService,UserRequestService] 
+})
+export class UserComponent implements OnInit {
+
+  user:User;
+  repository:Repository[];
+
+  userName=""
+
+  constructor(private usersService:UserRequestService) { 
+
+  }
+
+  submitUser(){
+    this.usersService.userRequest(this.userName)
+    this.usersService.repositoryrequest(this.userName) 
+
+  ngOnInit(): void {
+
+    this.usersService.userRequest('Joselyne97')
+      this.usersService.repositoryrequest('Joselyne97')
+           
+      this.user=this.usersService.user
+      this.repository=this.usersService.repository
+      console.log(this.repository)
+  }
+
+}
